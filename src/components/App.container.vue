4a04c04d68b2824b7809c2871e50e3005d1b44c2
saumya <2saumyaray@gmail.com>
2020-07-18 00:56:08 +0530

    wip: settign up the basics for UI

---
+++ src/components/App.container.vue
@@ -0,0 +1,10 @@
+<template>
+    <div id="app" class="section">
+        Vue + Electron
+    </div>
+</template>
+<script>
+export default {
+    name: 'AppContainer'
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
-import App from './App.vue'
+//import App from './App.vue'
+import AppContainer from './components/App.container'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(AppContainer),
 }).$mount('#app')
