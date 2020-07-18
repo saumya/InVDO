2bdeb732c64331ae120c62793eead7b33bcd6f12
saumya <2saumyaray@gmail.com>
2020-07-18 17:42:50 +0530

    UI design for the home page

--- src/components/Nav1.comp.vue
+++ src/components/Nav1.comp.vue
@@ -3,21 +3,41 @@
         Nav1.comp.vue
         <section>
             <div class="field has-addons">
-                <p class="control"> <button class="button">Info</button> </p>
-                <p class="control"> <button class="button">Popular</button> </p>
-                <p class="control"> <button class="button">Trending</button> </p>
+                <p class="control"> <button class="button" v-on:click="onInfoClick">Info</button> </p>
+                <p class="control"> <button class="button" v-on:click="onPopularClick">Popular</button> </p>
+                <p class="control"> <button class="button" v-on:click="onTrendingClick">Trending</button> </p>
             </div>
             <div class="field has-addons">
                 <p class="control">
-                    <input class="input" type="text" placeholder="Video">
+                    <input class="input" type="text" placeholder="Video" v-model="searchString" />
                 </p>
-                <p class="control"> <button class="button">Search</button> </p>
+                <p class="control"> <button class="button" v-on:click="onSearchClick">Search</button> </p>
             </div>
         </section>
     </div>
 </template>
 <script>
 export default {
-    name: 'Nav_1_Component'
+    name: 'Nav_1_Component',
+    data: function(){
+        return({
+            searchString:'video'
+        })
+    },
+    methods: {
+        onInfoClick(){
+            console.log('info')
+        },
+        onPopularClick(){
+            console.log('popular')
+        },
+        onTrendingClick(){
+            console.log('Trending')
+        },
+        onSearchClick(){
+            console.log('onSearchClick')
+            console.log( this.searchString )
+        }
+    }
 }
 </script>
