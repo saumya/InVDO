eed4b75f440689823aa46b4fa24bdb1a0208c927
saumya <2saumyaray@gmail.com>
2020-07-24 22:39:55 +0530

    wip: search result list is displayed.

--- src/components/Search.comp.vue
+++ src/components/Search.comp.vue
@@ -3,19 +3,29 @@
         <h1 class="title"> Search </h1>
         <h2 class="subtitle">Search Youtube</h2>
         <Nav1Comp />
-        
+        <SearchListItemComp />
+        <!--
         <section class="section">
-            {{ getSearchList }}
+            <ul id="popular_list" style="height:300px; overflow:scroll">
+            <li v-for="(item,index) in getSearchList" :key="index">
+                
+                {{ JSON.stringify(item) }}
+                <span>----------------------</span>
+                <br/>
+            </li>
+        </ul>
         </section>
+        -->
     </section>
 </template>
 <script>
 import {mapGetters} from 'vuex'
 import Nav1Comp from './Nav1.comp'
+import SearchListItemComp from './SearchListItem.comp'
 
 export default {
     name: 'SearchComponent',
-    components: { Nav1Comp },
+    components: { Nav1Comp, SearchListItemComp },
     computed: {
         ...mapGetters([
             'getSearchList'
---
+++ src/components/SearchListItem.comp.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <section class="section">
+            <ul id="popular_list" style="height:300px; overflow:scroll">
+                <li v-for="(item,index) in getSearchList" :key="index">
+                    <div class="mb-4">
+                        <div><strong>{{ item.title }}</strong></div>
+                        <div>{{ item.description }}</div>
+                        <!--<div>{{ item.descriptionHtml }}</div>-->
+
+                        <div>Video Id-{{ item.videoId }}</div>
+                        <div>Author-{{ item.author }}</div>
+                        <div>Author Id-{{ item.authorId }}</div>
+                        <div>Author URL-{{ item.authorUrl }}</div>
+                        <div> ViewCount-{{item.viewCount}} </div>
+
+                        <div> LiveNow-{{item.liveNow?"Yes":"No"}}, Paid-{{item.paid?"Yes":"No"}}, Premium-{{item.premium?"Yes":"No"}}, isUpcoming-{{item.isUpcoming?"Yes":"No"}} </div>
+                        
+                    </div>
+                </li>
+            </ul>
+        </section>
+    </div>
+</template>
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+    name: 'SearchListItemComponent',
+    computed: {
+        ...mapGetters([
+            'getSearchList'
+        ])
+    }
+}
+</script>
